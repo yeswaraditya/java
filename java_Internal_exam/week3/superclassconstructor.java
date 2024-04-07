bed8294d206db6b0b293bdfec255a51967e34b02
Y Eswar Aditya <yeswaraditya3006@gmail.com>
2024-04-07 20:30:59 +0530

    java

--- java_Internal_exam/week3/superclassconstructor.java
+++ java_Internal_exam/week3/superclassconstructor.java
@@ -4,4 +4,22 @@ class Parent
 {
     public Parent()
     {
-        System.ou
+        System.out.println("Parent constructor");
+    }
+}
+
+class child extends Parent
+{
+    public child()
+    {
+        super();
+        System.out.println("child constructor");
+    }
+}
+
+public class superclassconstructor {
+    public static void main(String[] args)
+    {
+        new child();
+    }
+}
