cb5e8ef18c66054a5ae21258506b17e63c05f7a3
Y Eswar Aditya <yeswaraditya3006@gmail.com>
2024-04-07 18:53:03 +0530

    java

--- java_Internal_exam/week1/swap.java
+++ java_Internal_exam/week1/swap.java
@@ -3,4 +3,27 @@ import java.util.Scanner;
 class swap
 {
 
-    void swapnum(int a,int b
+    void swapnum(int a,int b)
+    {
+        int temp;
+        temp=a;
+        a=b;
+        b=temp;
+
+        System.out.println(" after swap a="+a+"b="+b);
+
+    }
+    public static void main(String[] args)
+    {
+        Scanner in=new Scanner(System.in);
+        System.out.println("enter first number");
+        int a=in.nextInt();
+        System.out.println("Enter second number");
+        int b=in.nextInt();
+
+        swap obj= new swap();
+        obj.swapnum(a,b);
+        in.close();
+
+    }
+}
